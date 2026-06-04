7cc6a5148edf9e22316e3b5bc3d73897a6052d24
Viinicius20 <viniciusamoury0403@gmail.com>
2026-06-04 15:04:33 -0300

    feat: upload multiplo de tokens

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -355,6 +355,7 @@ export default function Galeria() {
       campaign_id: CAMPANHA_ID,
       map_id: mapaAtivo.id,
       token_id: tokenId,
+      token_url: tokenUrl,
       x: Math.round(x),
       y: Math.round(y),
       label: ''
