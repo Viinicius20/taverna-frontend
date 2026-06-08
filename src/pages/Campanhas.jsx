ffa29dccabb6866f5d75eb26a3c513788bbc2a20
Viinicius20 <viniciusamoury0403@gmail.com>
2026-06-08 17:56:29 -0300

    feat: upload multiplo de tokens

--- src/pages/Campanhas.jsx
+++ src/pages/Campanhas.jsx
@@ -22,6 +22,7 @@ export default function Campanhas() {
 
   useEffect(() => {
     buscarCampanhas();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function buscarCampanhas() {
