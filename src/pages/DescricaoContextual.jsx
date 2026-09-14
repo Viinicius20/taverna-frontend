dabaf520ff97896657182232ca64aa6290f7a953
Viinicius20 <viniciusamoury0403@gmail.com>
2026-09-14 19:29:56 -0300

    Up descricao

--- src/pages/DescricaoContextual.jsx
+++ src/pages/DescricaoContextual.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import { useState, useEffect } from 'react';
+
 
 const cinzel = { fontFamily: "'Cinzel', serif" };
 const crimson = { fontFamily: "'Crimson Pro', serif" };
