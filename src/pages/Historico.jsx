c4bdc400399462aebf9dae816c97bec453ff93a2
Viinicius20 <viniciusamoury0403@gmail.com>
2026-09-20 01:13:38 -0300

    Ajuste

--- src/pages/Historico.jsx
+++ src/pages/Historico.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import api from '../services/api';
 
 const cinzel = { fontFamily: "'Cinzel', serif" };
 const crimson = { fontFamily: "'Crimson Pro', serif" };
